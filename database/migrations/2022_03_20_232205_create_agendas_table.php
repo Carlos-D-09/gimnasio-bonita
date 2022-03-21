6bd2b7ad8911792589b4c2a34792e2d67ae558f2
Carlos Daniel Medina Sahagun <carlos.medina1311@alumnos.udg.mx>
2022-03-20 20:54:47 -0600

    Migración de la tabla agendas

---
+++ database/migrations/2022_03_20_232205_create_agendas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agendas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_cliente');
+            $table->unsignedBigInteger('id_oferta');
+            $table->foreign("id_cliente")
+                ->references("id")->on("clientes");
+            $table->foreign("id_oferta")
+                ->references("id")->on("oferta_actividades");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agendas');
+    }
+}
