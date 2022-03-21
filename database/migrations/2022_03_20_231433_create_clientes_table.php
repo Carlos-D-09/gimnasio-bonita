7409dc3988a9449c391fc7a65d4c887c74b49f9b
Carlos Daniel Medina Sahagun <carlos.medina1311@alumnos.udg.mx>
2022-03-20 20:12:33 -0600

    Migración de la tabla clientes

---
+++ database/migrations/2022_03_20_231433_create_clientes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 150);
+            $table->date('fecha_nacimiento');
+            $table->string('domicilio', 100);
+            $table->string('telefono',13);
+            $table->string('correo', 50);
+            $table->date('fecha_registro');
+            $table->boolean('status');
+            $table->string('password', 150);
+            $table->unsignedBigInteger('id_empleado');
+            $table->foreign("id_empleado")
+                ->references("id")->on("empleados");
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
