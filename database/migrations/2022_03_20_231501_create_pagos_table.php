fc80597737bbcd6138fc0e1d5ac90d0ba25ffef7
Carlos Daniel Medina Sahagun <carlos.medina1311@alumnos.udg.mx>
2022-03-20 20:15:39 -0600

    Migración de la tabla pagos

---
+++ database/migrations/2022_03_20_231501_create_pagos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePagosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pagos', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nombre',100);
+            $table->date('fecha');
+            $table->integer('dias')->NULL;
+            $table->double('monto',8,2);
+            $table->unsignedBigInteger('id_membresia');
+            $table->unsignedBigInteger('id_empleado');
+            $table->unsignedBigInteger('id_cliente');
+            $table->foreign("id_membresia")
+                ->references("id")->on("membresia");
+            $table->foreign("id_empleado")
+                ->references("id")->on("empleados");
+            $table->foreign("id_cliente")
+                ->references("id")->on("clientes");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pagos');
+    }
+}
